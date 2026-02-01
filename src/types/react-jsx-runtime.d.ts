5e25258fb0ec1bd7c8b7dbf833e061fb864956cd
0974243312 <chinithem81@gmail.com>
2026-02-01 15:29:09 +0700

    Update vehicles page + fix dev server issues

---
+++ src/types/react-jsx-runtime.d.ts
@@ -0,0 +1,5 @@
+declare module "react/jsx-runtime" {
+  export function jsx(type: unknown, props: unknown, key?: string): unknown;
+  export function jsxs(type: unknown, props: unknown, key?: string): unknown;
+  export const Fragment: unknown;
+}
